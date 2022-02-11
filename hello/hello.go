d9758cb5f32d4c469894a425af1e30bcb33e22c4
qwertytoki <qwertytoki@gmail.com>
2022-02-11 23:37:07 +0800

    迷走中

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,12 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
-
-	"greetings"
+	"../greetings"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	fmt.Println(quote.Go())
+	// fmt.Println(quote.Go())
 
 	// Get a greeting message and print it
 	message := greetings.Hello("Taro")
