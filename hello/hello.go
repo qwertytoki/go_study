9e843b52def0984b404e25e405242786813636fc
qwertytoki <qwertytoki@gmail.com>
2022-02-11 23:13:40 +0800

    hmm something wrong...

--- hello/hello.go
+++ hello/hello.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 
 	"rsc.io/quote"
+
+	"greetings"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println(quote.Go())
+
+	// Get a greeting message and print it
+	message := greetings.Hello("Taro")
+	fmt.Println(message)
+
 }
