76d48d36cee9513396b7c570dbb86cd25c4cd8bd
qwertytoki <qwertytoki@gmail.com>
2022-02-13 12:55:05 +0800

    error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
+
 	fmt.Println("Hello World")
 	fmt.Println(quote.Go())
 
 	// Get a greeting message and print it
-	message := greetings.Hello("Taro")
-	fmt.Println(message)
+	// message := greetings.Hello("Taro")
+	// fmt.Println(message)
 
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was retruned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message to the console.
+	fmt.Println(message)
 }
