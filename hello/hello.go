2ef1ff5c1b7d3e1212579ca5493dc3e6190709b5
qwertytoki <qwertytoki@gmail.com>
2022-02-13 14:22:14 +0800

    errror handling

--- hello/hello.go
+++ hello/hello.go
@@ -10,6 +10,9 @@ import (
 
 func main() {
 
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
 	log.SetPrefix("greetings:")
 	log.SetFlags(0)
 
